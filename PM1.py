74c7d62acd31e419a9adf3b274e0f98b6fb0297a
ahmadhany99 <ahmadhany1999@hotmail.com>
2022-10-03 15:12:16 -0400

    Tasks 2.1 and 2.2 done

--- PM1.py
+++ PM1.py
@@ -1,6 +1,10 @@
 import json as js
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn as sk
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 f = open('goemotions.json')
 y = js.load(f)
 emotions = {}
@@ -20,4 +24,9 @@ plt.legend(loc='center left',bbox_to_anchor=(-0.4,0.5),fontsize=8)
 plt.show()
 plt.pie(sentiment.values(),labels=sentiment.keys())
 plt.show()
+a=np.concatenate(y)
+vec=CountVectorizer()
+vec.fit(a)
+print (len(vec.vocabulary_))
+train, test = train_test_split(y,train_size=0.8, shuffle=True, test_size=0.2)
 f.close()
