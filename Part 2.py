46578e07f61346572a9f982c70926ae8d7c82380
mohona6646 <60407634+mohona6646@users.noreply.github.com>
2022-10-10 18:08:37 -0400

    Revert "Part_2.3.1 done"
    This reverts commit 156671d489ca96f392979b71f6527d749ff8211e.

--- Part 2.py
+++ Part 2.py
@@ -3,45 +3,23 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import tree
-from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import metrics
-
-#Opening and loading the dataset
-file = open('goemotions.json')
-fLoaded = js.load(file)
-#print(fLoaded)
-
-#PART 2.1
-posts = [item[0] for item in fLoaded]
-vec = CountVectorizer()
-vec.fit(posts)
-print(len(vec.vocabulary_))
-
-#PART 2.2
-emotions = [item[1] for item in fLoaded]
-sentiments = [item[2] for item in fLoaded]
-
-X_train, X_test, yEmotion_train, yEmotion_test, ySent_train, ySent_test= train_test_split(posts, emotions, sentiments,train_size=0.80,
-                                                                     shuffle=True, test_size=0.20)
-#Part 2.3.1
-X_train = vec.fit_transform(X_train)
-X_test = vec.transform(X_test)
-
-classifier = MultinomialNB()
-model = classifier.fit(X_train, yEmotion_train)
-
-y_pred = model.predict(X_test)
-print("For emotions : \n")
-print(classification_report(yEmotion_test, y_pred, zero_division= 1))
-print("Confusion Matrix: \n", confusion_matrix(yEmotion_test, y_pred))
-
-model = classifier.fit(X_train, ySent_train)
-y_pred = model.predict(X_test)
-print("For sentiments : \n")
-print(classification_report(ySent_test, y_pred, zero_division= 1))
-print("Confusion Matrix: \n", confusion_matrix(ySent_test, y_pred))
-file.close()
+from sklearn.feature_extraction.text import CountVectorizer
+f = open('goemotions.json')
+y = js.load(f)
+emotions = {}
+sentiment={}
+for i in y:
+    if i[1] not in emotions:
+        emotions[i[1]] = 1
+    else:
+        emotions[i[1]] += 1
+    if i[2] not in sentiment:
+        sentiment[i[2]] = 1
+    else:
+        sentiment[i[2]] += 1
+a=np.concatenate(y)
+vec=CountVectorizer()
+vec.fit(a)
+print (len(vec.vocabulary_))
+train, test = train_test_split(y,train_size=0.8, shuffle=True, test_size=0.2)
